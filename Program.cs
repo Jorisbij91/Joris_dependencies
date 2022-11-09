33751333dd60f44a6c00dfe64bd0d246d5d0b4ec
Jorisbij91 <86472788+Jorisbij91@users.noreply.github.com>
2022-11-09 02:15:50 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace c_sharp_dot_net_3._1_console_sonar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
